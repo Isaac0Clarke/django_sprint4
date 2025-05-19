a535930848a9599ebcca1a86eaeb13ac9705fb03
Isaac9Clarke <138529944+Isaac0Clarke@users.noreply.github.com>
2025-05-20 02:29:36 +0300

    Delete blog directory

--- blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Location, Post
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
